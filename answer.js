55e21b623bd59f68f5d55de7a7a43f36e1231518
Alicia Wilson <alicia@thearrayschool.com>
2018-12-13 15:56:50 -0700

    finished challenge 3

--- answer.js
+++ answer.js
@@ -1,1 +1,15 @@
 // *** YOUR ANSWER BELOW ***
+
+function divisibleBy6Or7(){
+  let divBy6 = "";
+  let divBy7 = "";
+  for(let i = 1; i <= 100; i++){
+    if(i % 6 === 0 && !(i % 7 === 0)){
+      divBy6 += `${i}, `;
+    }
+    if(i % 7 === 0 && !(i % 6 === 0)){
+      divBy7 += `${i}, `
+    }
+  }
+  return divBy6 + divBy7;
+}
